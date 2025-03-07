42314a68bd1dd1fcc7aa4b7881674a637b2bd3b3
isaquesousa <isaqueluzsousa83@gmail.com>
2025-03-06 22:18:39 -0300

    tela de login Toast

--- app/src/main/java/br/com/etecia/myapp/LoginActivity.java
+++ app/src/main/java/br/com/etecia/myapp/LoginActivity.java
@@ -1,6 +1,8 @@
 package br.com.etecia.myapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
+    Button btnsair;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +23,16 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        btnsair = findViewById(R.id.btnsair);
+
+        btnsair = setOnClickListener(New View.OnClickListener(){
+            @Override
+                    public void onClick(View v){
+                Toast.makeText(getApplicationContext(),text "clique no sair", Toast.LENGTH_SHORT).show();
+            }
+
+         });
+
+        }
     }
 }
